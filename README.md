b33447b63cc78b3dc36c133b93826972b78a8445
MiguelJM <mikejm92@gmail.com>
2016-01-18 16:00:23 +0100

    Plasson with tests

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JenkinsTestOnPlasson
+# PlassonWebApp
 This test is used to verify that Plasson's test configuration is correct.
 
 Tests will be autommatically executed by the Jenkins Server as soon as a push command is sent to GitHub.
---
+++ WebApplicationPlasson/src/test/java/plasson/JsonHelperTest.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package plasson;
+
+import junit.framework.TestCase;
+import org.codehaus.jettison.json.JSONObject;
+
+/**
+ *
+ * @author Adirael
+ */
+public class JsonHelperTest extends TestCase {
+    
+    public JsonHelperTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of getJson method, of class JsonHelper.
+     */
+    public void testGetJson_Consumer() {
+        System.out.println("getJson");
+        Consumer consumer = null;
+        JsonHelper instance = new JsonHelper();
+        JSONObject expResult = null;
+        JSONObject result = instance.getJson(consumer);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getJson method, of class JsonHelper.
+     */
+    public void testGetJson_Provider() {
+        System.out.println("getJson");
+        Provider provider = null;
+        JsonHelper instance = new JsonHelper();
+        JSONObject expResult = null;
+        JSONObject result = instance.getJson(provider);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getGoJson method, of class JsonHelper.
+     */
+    public void testGetGoJson() {
+        System.out.println("getGoJson");
+        JsonHelper instance = new JsonHelper();
+        JSONObject expResult = null;
+        JSONObject result = instance.getGoJson();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getResults method, of class JsonHelper.
+     */
+    public void testGetResults() {
+        System.out.println("getResults");
+        String receivedResults = "";
+        JsonHelper instance = new JsonHelper();
+        Results expResult = null;
+        Results result = instance.getResults(receivedResults);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
